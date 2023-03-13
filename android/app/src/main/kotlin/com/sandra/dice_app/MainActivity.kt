efd77bc47b54f773002df2b25d46522c67bd4074
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-03-13 14:54:40 +0100

    created new flutter project

---
+++ android/app/src/main/kotlin/com/sandra/dice_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sandra.dice_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('Dicee'),
+        backgroundColor: Colors.blue,
+      ),
+      body: DicePage(),
+    ),
+  ));
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
